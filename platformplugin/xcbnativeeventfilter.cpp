149cc3de39b4d3ef6f701bd2c16d713408a9b89a
zccrs <ccrr1314@live.com>
2017-08-04 15:55:04 +0800

    Fix the touchpad after the natural scroll setting does not take effect
    Change-Id: I9942d4d2d97b8fb3d96fedf501109265d178356a

--- platformplugin/xcbnativeeventfilter.cpp
+++ platformplugin/xcbnativeeventfilter.cpp
@@ -10,6 +10,8 @@
 #include "dxcbwmsupport.h"
 
 #include <xcb/xfixes.h>
+#include <X11/extensions/XI2proto.h>
+#include <X11/extensions/XInput2.h>
 
 DPP_BEGIN_NAMESPACE
 
@@ -27,6 +29,20 @@ QClipboard::Mode XcbNativeEventFilter::clipboardModeForAtom(xcb_atom_t a) const
         return QClipboard::Clipboard;
     // not supported enum value, used to detect errors
     return QClipboard::FindBuffer;
+}
+
+typedef struct qt_xcb_ge_event_t {
+    uint8_t  response_type;
+    uint8_t  extension;
+    uint16_t sequence;
+    uint32_t length;
+    uint16_t event_type;
+} qt_xcb_ge_event_t;
+
+static inline bool isXIEvent(xcb_generic_event_t *event, int opCode)
+{
+    qt_xcb_ge_event_t *e = (qt_xcb_ge_event_t *)event;
+    return e->extension == opCode;
 }
 
 bool XcbNativeEventFilter::nativeEventFilter(const QByteArray &eventType, void *message, long *result)
@@ -69,6 +85,51 @@ bool XcbNativeEventFilter::nativeEventFilter(const QByteArray &eventType, void *
             emit DXcbWMSupport::instance()->windowListChanged();
         }
     }
+    // 修复Qt程序对触摸板的自然滚动开关后不能实时生效
+    // 由于在收到xi的DeviceChanged事件后，Qt更新ScrollingDevice时没有更新verticalIncrement字段
+    // 导致那些使用increment的正负值控制自然滚动开关的设备对Qt程序无法实时生效
+    // 有些电脑上触摸板没有此问题，是因为他的系统环境中没有安装xserver-xorg-input-synaptics
+#ifdef XCB_USE_XINPUT21
+    else if (response_type == XCB_GE_GENERIC) {
+        QXcbConnection *xcb_connect = DPlatformIntegration::xcbConnection();
+
+        if (xcb_connect->m_xi2Enabled && isXIEvent(event, xcb_connect->m_xiOpCode)) {
+            xXIGenericDeviceEvent *xiEvent = reinterpret_cast<xXIGenericDeviceEvent *>(event);
+
+            if (xiEvent->evtype != XI_DeviceChanged) {
+                return false;
+            }
+
+            xXIDeviceChangedEvent *xiDCEvent = reinterpret_cast<xXIDeviceChangedEvent *>(xiEvent);
+            QHash<int, QXcbConnection::ScrollingDevice>::iterator device = xcb_connect->m_scrollingDevices.find(xiDCEvent->sourceid);
+
+            int nrDevices = 0;
+            XIDeviceInfo* xiDeviceInfo = XIQueryDevice(static_cast<Display *>(xcb_connect->xlib_display()), xiDCEvent->sourceid, &nrDevices);
+
+            if (nrDevices <= 0) {
+                return false;
+            }
+
+            for (int c = 0; c < xiDeviceInfo->num_classes; ++c) {
+                if (xiDeviceInfo->classes[c]->type == XIScrollClass) {
+                    XIScrollClassInfo *sci = reinterpret_cast<XIScrollClassInfo *>(xiDeviceInfo->classes[c]);
+                    if (sci->scroll_type == XIScrollTypeVertical) {
+                        device->orientations |= Qt::Vertical;
+                        device->verticalIndex = sci->number;
+                        device->verticalIncrement = sci->increment;
+                    }
+                    else if (sci->scroll_type == XIScrollTypeHorizontal) {
+                        device->orientations |= Qt::Horizontal;
+                        device->horizontalIndex = sci->number;
+                        device->horizontalIncrement = sci->increment;
+                    }
+                }
+            }
+
+            XIFreeDeviceInfo(xiDeviceInfo);
+        }
+    }
+#endif
 
     return false;
 }
