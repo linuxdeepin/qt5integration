9c06e7709b99958e6ebb98714fbbfa2a61d7a0a5
zccrs <ccrr1314@live.com>
2017-12-05 15:22:49 +0800

    fix(style): visable left/top lines on draw rubber band control
    Change-Id: I558a473d6deee3e83982de2a44d010e0e99e16c6

--- dstyleplugin/rubberbandhelper.cpp
+++ dstyleplugin/rubberbandhelper.cpp
@@ -28,7 +28,7 @@ bool Style::drawRubberBandControl(const QStyleOption *option, QPainter *painter,
     const QBrush &backgroundBrush = m_palette->brush(PaletteExtended::RubberBand_BackgroundBrush, option);
     const QBrush &borderBrush = m_palette->brush(PaletteExtended::RubberBand_BorderBrush, option);
 
-    PainterHelper::drawRect(painter, option->rect.adjusted(0, 0, -1, -1),
+    PainterHelper::drawRect(painter, option->rect.adjusted(1, 1, -1, -1),
                             backgroundBrush, Metrics::Painter_PenWidth, borderBrush);
 
     return true;
