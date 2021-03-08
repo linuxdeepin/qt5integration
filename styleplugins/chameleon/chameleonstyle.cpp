c8049be94d4e559d8820ccf227d77ade6491a256
wangpeng <wangpeng@uniontech.com>
2021-03-08 16:58:13 +0800

    fix: 修复日历控件暗色主题下显示问题
    在暗色主题下年月按钮文本颜色与主题颜色相近,
    由于QT源码中设置了无效的调色板,故给予一个调色板
    
    Log:
    Bug: https://pms.uniontech.com/zentao/bug-view-65869.html
    Change-Id: Id99ecb0eedd19946aa59093a910eb461642c9425

--- styleplugins/chameleon/chameleonstyle.cpp
+++ styleplugins/chameleon/chameleonstyle.cpp
@@ -2956,7 +2956,7 @@ void ChameleonStyle::drawComplexControl(QStyle::ComplexControl cc, const QStyleO
             label.rect = button.adjusted(fw, fw, -fw, -fw);
 
             if (w && w->property("_d_calendarToolBtn").toBool()) {
-                label.palette.setColor(QPalette::ButtonText, QPalette::Highlight);
+                label.palette = DGuiApplicationHelper::instance()->standardPalette(DGuiApplicationHelper::instance()->themeType());
             }
 
             proxy()->drawControl(CE_ToolButtonLabel, &label, p, w);
