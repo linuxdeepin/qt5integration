926e35c053087f3aca9971d5bcc6dae5dab91dd4
AlexOne <wangpenga@uniontech.com>
2022-02-18 13:33:35 +0800

    chore: 修改单元测试生成目录
    修改单元测试相关文件的目录结构
    
    Log:
    Influence: 单元测试目录结构
    Change-Id: I35c55e284faa1c3b565700840f86959f1e7d996f

--- tests/test-recoverage-qmake.sh
+++ tests/test-recoverage-qmake.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -e
-BUILD_DIR=build
-REPORT_DIR=report
+BUILD_DIR=`pwd`/../build-ut
+HTML_DIR=${BUILD_DIR}/html
+XML_DIR=${BUILD_DIR}/report
 cd ../
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
@@ -16,15 +17,15 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 qmake ../ CONFIG+=debug BASED_DTK_DIR=based-dtk
 export ASAN_OPTIONS=halt_on_error=0
-TESTARGS="--gtest_output=xml:dde_test_report_qt5integration.xml" make check -j$(nproc)
+TESTARGS="--gtest_output=xml:${XML_DIR}/report_qt5integration.xml" make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 #lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
-lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" --output-file coverage.info
+lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build-ut/src*" --output-file coverage.info
 cd ..
-genhtml -o $REPORT_DIR $BUILD_DIR/coverage.info
+genhtml -o $HTML_DIR $BUILD_DIR/coverage.info && mv ${BUILD_DIR}/html/index.html ${BUILD_DIR}/html/cov_qt5integration.html
 
-test -e ./build/asan.log* && mv ./build/asan.log* ./build/asan_qt5integration.log || touch ./build/asan.log
+test -e ${BUILD_DIR}/asan.log* && mv ${BUILD_DIR}/asan.log* ${BUILD_DIR}/asan_qt5integration.log || touch ${BUILD_DIR}/asan_qt5integration.log
 
 #rm -rf $BUILD_DIR
 #rm -rf ../$BUILD_DIR
