b4605816eb4305780f04108fd903ded1ca814c43
xzl <1037981455@qq.com>
2022-08-09 10:34:02 +0800

    chore: adapt REUSE license header
    修改开源声明为REUSE格式
    
    Log: 开源合规修改

--- platformthemeplugin/diconengine.cpp
+++ platformthemeplugin/diconengine.cpp
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2018 - 2022 UnionTech Software Technology Co., Ltd.  
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 #include "diconengine.h"
 
 #include <themed_icon_lookup.h>
